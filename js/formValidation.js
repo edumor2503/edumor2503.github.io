7ee6b7e0d22f3ad411d473a876f55fd0f16b926c
Eduardo Moreno <167011235+edumor2503@users.noreply.github.com>
2024-06-24 16:16:29 -0300

    Add files via upload

--- js/formValidation.js
+++ js/formValidation.js
@@ -2,19 +2,22 @@ function validatePassword() {
     var password = document.getElementById('password').value;
     var confirm_password = document.getElementById('confirm_password').value;
     if (password !== confirm_password) {
-        alert("Los passwords no coinciden");
+        document.getElementById('customAlert').style.display = 'block'; 
         document.getElementById('error-message').style.display = 'block';
     } else {
-        // coinciden
         document.getElementById('error-message').style.display = 'none';
     }
 }
 
-// Agregar el evento 'submit' al formulario para prevenir el envío si las contraseñas no coinciden
+function closeCustomAlert() {
+    document.getElementById('customAlert').style.display = 'none';
+  }
+  
 document.getElementById('registrar').addEventListener('submit', function(event) {
     var password = document.getElementById('password').value;
     var confirm_password = document.getElementById('confirm_password').value;
     if (password !== confirm_password) {
         event.preventDefault();
+        validatePassword(); 
     }
 });
